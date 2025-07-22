6ceac821eaffb585a9dd51b74fd58e5d44299e98
komikasinaduni <kkomalini@gmail.com>
2025-07-21 23:59:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-});
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('header nav a');
-
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop -150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if(top>=offset &&top<offset+height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-            });
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
         }
-    });
 
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-};
+    })
 
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
@@ -57,3 +49,14 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar   = document.querySelector('.navbar');
+  
+    menuIcon.addEventListener('click', () => {
+      menuIcon.classList.toggle('bx-x');
+      navbar.classList.toggle('active');
+    });
+  });
+  
