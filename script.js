2e34f0433e1544fee23ca7b787c09a3a2c1ee01d
komikasinaduni <kkomalini@gmail.com>
2025-07-21 23:51:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
+menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-};
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+});
 
 window.onscroll = () => {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop -150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top>=offset &&top<offset+height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
         }
+    });
 
-    })
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
 
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
